51824264cbbaecab0415417ea767a7b9c48016a3
John Doe <johndoe@email.com>
2024-03-03 11:13:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
 import logo from './logo.svg';
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+import Navbar from './components/Navbar.js';
 import './App.css';
+//import "./components/Navbar.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route , Navigate} from 'react-router-dom';
+import "bootstrap/js/src/collapse.js";
 
+
+
+
+import Main from './Pages/mainPage.jsx';
+import Team from './Pages/team.jsx'; // Assuming this is where your /team route is defined
+import Layout from './Pages/Layout.js';
+import Projects from './Pages/projects.jsx';
+import Contact from './Pages/contact.jsx'
+import Events from './Pages/events.jsx';
+import Opportunities from './Pages/opportunities.jsx'
+import Newsletter from './Pages/newsletter.jsx'
+
+/*
+
+*/
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"> 
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+&nbsp;&nbsp;
+  <Link class="navbar-brand  mr-auto" style={{ marginRight: '10px',display:"inline-flex !important"}} to="/">   
+  <img
+    
+src="https://github.com/HoumanEbrahimi/Lol/blob/main/TMI.png?raw=true"
+alt=""
+width="45"
+height="45"
+
+/>
+  </Link>
+
+  <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Home <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/team" >Team</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/events" >Events</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/projects" >Projects</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/newsletter"  >Newsletter</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/opportunities" >Opportunities</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/contact">Contact Us</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+      
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Main />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path='/opportunities' element={<Opportunities/>} />
+        <Route path='/newsletter' element={<Newsletter />} />
+
+      </Routes>
+    </div> 
+
+    //<BrowserRouter>
+
+ // </BrowserRouter>
   );
 }
 
